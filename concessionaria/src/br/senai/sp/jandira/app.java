6337cb46c2adf1e6574fdde8432a1f0a7e954bc2
izaiasn <61168322+izaiasn@users.noreply.github.com>
2023-09-12 16:31:19 -0300

    Add files via upload

--- concessionaria/src/br/senai/sp/jandira/app.java
+++ concessionaria/src/br/senai/sp/jandira/app.java
@@ -1,12 +1,15 @@
 package br.senai.sp.jandira;
 
 import br.senai.sp.jandira.model.Cliente;
+import br.senai.sp.jandira.model.Menu;
 import br.senai.sp.jandira.model.Veiculo;
 
 public class app {
     public static void main(String[] args) {
-        Veiculo newObjveiculo = new Veiculo();
-        newObjveiculo.CadastrarVeiculo();
+        Menu newObjMenu = new Menu();
+        newObjMenu.menu();
+
+
 
     }
 }
